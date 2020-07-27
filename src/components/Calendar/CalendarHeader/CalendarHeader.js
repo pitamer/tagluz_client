eccd27fa96da5cfbfbd020165508b308d517a90c
Pita <itamarga@gmail.com>
2020-07-27 18:36:20 +0300

    safari fix for the last time?

--- src/components/Calendar/CalendarHeader/CalendarHeader.js
+++ src/components/Calendar/CalendarHeader/CalendarHeader.js
@@ -3,7 +3,9 @@ import Grid from "@material-ui/core/Grid";
 import * as dateFns from "date-fns";
 import { useStoreState, useStoreActions } from "easy-peasy";
 
-import MonthNavArrow from "./MonthNavArrow";
+import { makeStyles } from "@material-ui/core/styles";
+
+// import MonthNavArrow from "./MonthNavArrow";
 
 import "./index.css";
 
@@ -13,25 +15,45 @@ function CalendarHeader() {
   const nextMonth = useStoreActions((actions) => actions.nextMonth);
 
   const dateFormat = "MMMM yyyy";
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      textAlign: "center",
+      // display: 'flex'
+    },
+    item: {
+      justifyContent: "center",
+      // textAlign: "center",
+      display: "flex",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="header">
-      <Grid container spacing={3.5}>
+    <div className={`header ${classes.root}`}>
+      <Grid container spacing={3}>
         <Grid item xs>
-          <MonthNavArrow
-            direction="left"
-            action={() => prevMonth(currentMonth)}
-          />
+          <div
+            className={`icon ${classes.item}`}
+            onClick={() => prevMonth(currentMonth)}
+          >
+            chevron_left
+          </div>
         </Grid>
         <Grid item xs={6}>
-          <div className="col-center header-title">
+          <div className={`header-title`}>
             <span>{dateFns.format(currentMonth, dateFormat)}</span>
           </div>
         </Grid>
         <Grid item xs>
-          <MonthNavArrow
-            direction="right"
-            action={() => nextMonth(currentMonth)}
-          />
+          <div
+            className={`icon ${classes.item}`}
+            onClick={() => nextMonth(currentMonth)}
+          >
+            chevron_right
+          </div>
         </Grid>
       </Grid>
     </div>
